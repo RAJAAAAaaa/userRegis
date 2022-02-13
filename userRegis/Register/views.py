1606d0504c7432fe7d17bbacac8059bc0c2922b9
rajaa <rajaa.dolani@kuwaitnet.dev>
2022-02-13 10:09:01 +0200

    register

--- userRegis/Register/views.py
+++ userRegis/Register/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def Regist(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'Register/register.html', {'form': form})
--- userRegis/userRegis/settings.py
+++ userRegis/userRegis/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
     'Register',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user',
     'simple_history',
 
 
